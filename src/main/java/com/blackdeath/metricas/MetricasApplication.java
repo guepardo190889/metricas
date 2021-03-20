d9122e111a29e2a8bff8808af94aba03d0dbb35c
Seth Karim Luis Martínez <ing.seth.luis@gmail.com>
2021-03-20 11:36:50 -0600

    Commit inicial del proyecto

---
+++ src/main/java/com/blackdeath/metricas/MetricasApplication.java
@@ -0,0 +1,13 @@
+package com.blackdeath.metricas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricasApplication.class, args);
+	}
+
+}
